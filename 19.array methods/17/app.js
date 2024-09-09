cb2d0b0e6a28b0a00c0878088b913cb15090c1d7
KseniaMychkova <k2606676@gmail.com>
2024-09-09 21:39:55 +0300

    new change to 19 folder

--- 19.array methods/17/app.js
+++ 19.array methods/17/app.js
@@ -1,3 +1,13 @@
 // 17. Напишите функцию, которая принимает любое количество строковых аргументов и возвращает массив этих строк в верхнем регистре. Используйте оператор rest. 
 // Входные: toUpperCase('apple', 'banana', 'cherry') → Результат: ['APPLE', 'BANANA', 'CHERRY'] 
 // Входные: toUpperCase('dog', 'cat') → Результат: ['DOG', 'CAT']
+
+
+function toUpperCase(...arr) {
+    const result = arr.map(function(el) {
+        return el.toUpperCase();
+    })
+    console.log(result);
+
+}
+toUpperCase('dog', 'cat')
